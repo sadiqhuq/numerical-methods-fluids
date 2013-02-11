802368e206daf2ab05852271911672f64a49ce7c
Sadiq Huq <sadiqhuq@gmail.com>
2013-02-11 02:50:44 +0100

    bug fix

--- onedconvec.py
+++ onedconvec.py
@@ -8,7 +8,7 @@ def explicit_conv_1d ( phi, dt, U0, npoints, phinew ):
 
     # # Loop over points in space
     # for j  in range (1, npoints):
-    for j  in range (0, npoints):
+    for j  in range (1, npoints):
         phinew[j] = phi[j] - ( U0*dt/2./dx ) * ( phi[j+1] - phi[j-1] )
 
     # # Periodic BC at x = 2 pi
@@ -16,11 +16,14 @@ def explicit_conv_1d ( phi, dt, U0, npoints, phinew ):
 
     return phinew
 
-def conv_analytical_1d ( t, npoints, phi_a ):
-    # # Analytical solution
-    for j in range( npoints+1):
-        # t = i * dt
-        phi_a[j] = np.sin(x[j]-(U0*t))
+def conv_analytical_1d ( t, npoints, phi_a, case ):
+    
+    if ( case == 'sine' ):
+     # # Analytical solution
+       for j in range( npoints+1):
+           # t = i * dt
+           phi_a[j] = np.sin( x[j]-(U0*t) )
+    
     return phi_a
       
 npoints = 40
@@ -31,30 +34,43 @@ dt      = 0.001
 tsteps  = 1000
 
 # # Initial Condition
-phi     = np.sin(x)        # phi(x,t) = sin(x) at t=0
-
+phi0     = np.sin(x)        # phi(x,t) = sin(x) at t=0
+# phi0      = np.exp(-((x-np.pi)/0.01)**2) # impulse
 # # Explicit Euler:
 # # phinew is phi at time n+1
 
+phi    = phi0
 phinew = np.zeros(np.shape(phi))
+phi_a  = np.zeros(np.shape(phi))
 
 for i in range(tsteps+1):
 
   phinew = explicit_conv_1d ( phi, dt, U0, npoints, phinew )
-
   # # Write new phi to old phi
   phi = phinew
   
-  phi_a = conv_analytical_1d (i*dt, npoints, phi_a )
+  phi_a = conv_analytical_1d (i*dt, npoints, phi_a, 'sine' )
+
+# # Implicit Euler:
+# # phinew is phi at time n+1
+
+phinew = np.zeros(np.shape(phi))
+
+
+#for i in range(tsteps+1):  
+#  phinew = implicit_conv_1d ( phi, dt, U0, npoints, phinew )
+#  # # Write new phi to old phi
+#  phi = phinew  
+#  phi_a = conv_analytical_1d (i*dt, npoints, phi_a )
 
 
   # Wave Transport Anmiation
   # plt.plot(x, phi, 'r-', x, phi_a, 'k-')
   # plt.show()
 
-plt.plot(x, np.sin(x), 'k.',label='t=0')
+plt.plot(x, phi0, 'k.-',label='t=0')
 plt.plot(x, phi_a, 'k-', label='Analytical')
-plt.plot(x, phi,   'r-', label='Explicit Euler')
+plt.plot(x, phi,   'ro-', label='Explicit Euler')
 
 plt.legend()
 
